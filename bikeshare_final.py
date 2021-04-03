3ed6e9e867d5063e83028048cf88789717149055
Tobias Graf <tobiasgraf01990@gmail.com>
2021-04-04 00:33:41 +0200

    Revise Docstrings

--- bikeshare_final.py
+++ bikeshare_final.py
@@ -13,7 +13,7 @@ def get_filters():
     Returns:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        (str) dow - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
 
@@ -63,9 +63,9 @@ def load_data(city, month, dow):
     Args:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        (str) dow - name of the day of week to filter by, or "all" to apply no day filter
     Returns:
-        df - Pandas DataFrame containing city data filtered by month and day
+        df_citydata - Pandas DataFrame containing city data filtered by month and day
     """
     df_citydata = pd.read_csv(CITY_DATA[city])
 
